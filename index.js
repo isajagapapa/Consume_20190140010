0eb4f70a7bcba9c236526ca619ee990bb2335089
isss7 <faisfidi@gmail.com>
2022-01-06 03:48:25 +0700

    FIX

---
+++ index.js
@@ -0,0 +1,19 @@
+function onSignIn(googleUser) {
+	var profile = googleUser.getBasicProfile();
+	localStorage.setItem("nama",profile.getName());
+	localStorage.setItem("image",profile.getImageUrl());
+	localStorage.setItem("email", profile.getEmail());
+	window.location.href="/access.html"
+}
+
+function onLoad(){
+	gapi.load("auth2", function(){
+			gapi.auth2.init();
+
+	})
+}
+
+$("#user").ready(function () {
+  var user = document.getElementById("user");
+  user.innerHTML = `Hallo, ${localStorage.getItem("nama")}`;
+});
